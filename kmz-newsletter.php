1d45db028abb9319cef6e4e9d684c7b80a3a4b0a
Vladimir Kamuz <v.kamuz@gmail.com>
2021-02-26 00:33:07 +0200

    Added simple static Gutenberg Block

---
+++ kmz-newsletter.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: KMZ Newsletter
+ * Description: Example demonstrating how to build first Gutenberg block
+ * Author: Vladimir Kamuz
+*/
+
+/**
+ * Admin CSS and JavaScript
+ */
+function kmz_block_assets(){
+    wp_enqueue_script(
+        'kmz-newsletter', plugin_dir_url( __FILE__ ) . 'assets/block-newsletter.js', array( 'wp-blocks', 'wp-element' ), dirname( __FILE__ ) . '/assets/block-newsletter.js' );
+    wp_enqueue_style(
+        'kmz-newsletter-css', plugin_dir_url( __FILE__ ) . 'assets/block-newsletter.css', array( 'wp-edit-blocks' ), dirname( __FILE__ ) . '/assets/block-newsletter.css' );
+}
+add_action( 'enqueue_block_editor_assets', 'kmz_block_assets' );
+
+/**
+ * Front CSS and JavaScript
+ */
+function kmz_block_front_end_assets(){
+    wp_enqueue_style( 'wp-block-kmz-newsletter-css', plugin_dir_url( __FILE__ ) . 'assets/newsletter.css' );
+}
+add_action( 'wp_enqueue_scripts', 'kmz_block_front_end_assets' );
